63c5ee88a5a08a613527c2ec7db54a1837930585
Roman Moskalenko <skalermo@gmail.com>
2021-04-17 13:40:25 +0200

    Allow to use traits and types from opencv

--- src/opencv_allowed.rs
+++ src/opencv_allowed.rs
@@ -1,4 +1,4 @@
 pub use opencv::highgui::{ imshow, wait_key };
 pub use opencv::imgcodecs::{ imread, imwrite };
-pub use opencv::core::Mat;
+pub use opencv::core::{ Mat, MatTrait, MatExprTrait, Vec3b };
 pub use opencv::Error;

6d9b7f8605429c8e0063ecda4cc99f6aae5d1497
Roman Moskalenko <skalermo@gmail.com>
2021-04-18 22:04:57 +0200

    Use filters

--- src/bin.rs
+++ src/bin.rs
@@ -1,12 +1,14 @@
-use logo_lib::opencv_allowed::{imshow, imread, wait_key};
-use logo_lib::{convert_colors, BGR2HSV};
+use logo_lib::opencv_allowed::{imshow, imread, wait_key, Mat};
+#[allow(unused_imports)]
+use logo_lib::{convert_colors, convolve, BGR2HSV};
+use logo_lib::{DefaultKernels, Blur};
 
 fn main() {
-    // assert_eq!([87, 227, 189], bgr2hsv([170, 189, 21]));
-
     let filename = "data/1.jpg";
     let image = imread(filename, 1).unwrap();
-    let res = convert_colors(&image, BGR2HSV).unwrap();
+    // let res = convert_colors(&image, BGR2HSV).unwrap();
+    let mut res = Mat::default().unwrap();
+    convolve(&image, &mut res, DefaultKernels::make_kernel_for(Blur)).unwrap();
     imshow("image", &image).unwrap();
     imshow("res", &res).unwrap();
     wait_key(0).unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 pub mod opencv_allowed;
 mod colors;
+mod filters;
+mod kernels;
+mod effects;
 
-pub use colors::convert_colors;
-pub use colors::Conversion::{BGR2HSV};
+pub use colors::{convert_colors, Conversion::BGR2HSV};
+pub use effects::convolve;
+pub use kernels::DefaultKernels::{self, Blur};
 
 #[cfg(test)]
 #[path = "../tests/tests.rs"]

0f15aadb3e2e013e92473181c736f5524b255b2d
Roman Moskalenko <skalermo@gmail.com>
2021-05-02 12:06:48 +0200

    Add naive implementation of Rank filters

--- src/bin.rs
+++ src/bin.rs
@@ -1,14 +1,16 @@
 use logo_lib::opencv_allowed::{imshow, imread, wait_key, Mat};
 #[allow(unused_imports)]
-use logo_lib::{convert_colors, convolve, BGR2HSV};
-use logo_lib::{DefaultKernels, Blur};
+use logo_lib::{convert_colors, convolve, apply_rank_filter, BGR2HSV};
+use logo_lib::{DefaultKernels, Blur, MedianFilter, MinFilter, MaxFilter};
 
 fn main() {
-    let filename = "data/1.jpg";
+    let filename = "data/Lena.png";
     let image = imread(filename, 1).unwrap();
     // let res = convert_colors(&image, BGR2HSV).unwrap();
     let mut res = Mat::default().unwrap();
-    convolve(&image, &mut res, DefaultKernels::make_kernel_for(Blur)).unwrap();
+
+    // convolve(&image, &mut res, DefaultKernels::make_kernel_for(Blur)).unwrap();
+    apply_rank_filter(&image, &mut res, 5, MedianFilter).unwrap();
     imshow("image", &image).unwrap();
     imshow("res", &res).unwrap();
     wait_key(0).unwrap();
--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,8 @@ mod kernels;
 mod effects;
 
 pub use colors::{convert_colors, Conversion::BGR2HSV};
-pub use effects::convolve;
+pub use filters::rank_filter::Mode::{MinFilter, MedianFilter, MaxFilter};
+pub use effects::{convolve, apply_rank_filter};
 pub use kernels::DefaultKernels::{self, Blur};
 
 #[cfg(test)]

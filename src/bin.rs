47960e0a6d40cd883f72afc2337411a79a11c680
Roman Moskalenko <skalermo@gmail.com>
2021-06-08 07:53:27 +0200

    Detect yellowish colors

--- src/bin.rs
+++ src/bin.rs
@@ -15,6 +15,9 @@ fn main() {
     let blue_mask = in_range(&hsv, (80, 50, 50), (130, 255, 255)).unwrap();
     let red_mask = in_range(&hsv, (0, 50, 50), (10, 255, 255)).unwrap();
     let red_mask2 = in_range(&hsv, (160, 50, 50), (180, 255, 255)).unwrap();
+    let mut orange_mask = in_range(&hsv, (20, 100, 20), (35, 255, 255)).unwrap();
+    orange_mask = dilation(&mut orange_mask, 3, 2).unwrap();
+    // orange_mask = opening(&mut orange_mask, 3, 1).unwrap();
     // let red = mask_or(&red_mask, &red_mask2).unwrap();
     // let filtered_red_mask = erosion(&red, 3, 1).unwrap();
     let filtered_blue_mask = closing(&blue_mask, 3, 1).unwrap();
@@ -23,6 +26,7 @@ fn main() {
     imshow("blue", &blue_mask).unwrap();
     imshow("fblue", &filtered_blue_mask).unwrap();
     imshow("bb_test", &bb_test).unwrap();
+    imshow("orange", &orange_mask).unwrap();
     // imshow("red", &red).unwrap();
     // imshow("fred", &filtered_red_mask).unwrap();
     wait_key(0).unwrap();
